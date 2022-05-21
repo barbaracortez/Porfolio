c2601a1fda7d05baedd87d79f05e02c8cb8ddf5c
Barbara Cortez <cortezbarbara.dev@gmail.com>
2022-05-21 01:43:05 -0300

    second section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  onDownloadClick(): void {
+    console.log("click en descargas de cv");
+    const downloadInstance = document.createElement('a');
+    downloadInstance.href = 'http://localhost:4200/assets/barbaraCortezCV.pdf';
+    downloadInstance.target = '_blank';
+    downloadInstance.download = 'Barbara Cortez CV';
+
+    document.body.appendChild(downloadInstance);
+    downloadInstance.click();
+    document.body.removeChild(downloadInstance);
+  }
 }
